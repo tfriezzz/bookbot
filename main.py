bb9b6c0adb718e07ebe40b1abd1edd882b1e2cf7
tfriezz <roswall.tim@gmail.com>
2024-08-23 00:10:20 +0200

    add word_count

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
+    file_words = file_contents.split()
+    word_count = len(file_words)
+    print(word_count)
+
 main()
